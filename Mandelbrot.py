c842401f07965ea07ddef2476d9cfd0a84e75af3
Tansy Arron <tansy.arron@gmail.com>
2014-10-05 17:43:51 -0400

    Added image render with Pillow

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,3 +1,5 @@
+import PIL.Image
+
 def in_set(C):
 	Z = 0
 	for iteration in range(1, 600):
@@ -10,17 +12,17 @@ def get_complex(x, y, width, height):
 	return complex(-2 + 3.0/width * x, 1 - 2.0/height * y)
 
 def render(width, height):
+	mandel = PIL.Image.new('RGBA', (width, height))
 	for y in range(height):
 		row = []
 		for x in range(width):
 			C = get_complex(x, y, width, height)
 			if in_set(C):
-				row.append('*')
-			else:
-				row.append(' ')
-		print ' '.join(row)
+				mandel.putpixel((x,y), (255, 255, 255, 0))
+	mandel.show()
 
-print render(120,80)
+print render(1200,800)
+
 
 
 
