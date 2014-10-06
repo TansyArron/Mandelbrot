d8472de23852e1d6ec27d5959fd23a90fefd03a2
Tansy Arron <tansy.arron@gmail.com>
2014-10-06 11:04:39 -0400

    Changed color format to HSV, added doc strings.

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,6 +1,15 @@
+'''
+renders mandelbrot set.
+'''
+
 import PIL.Image
 
 def in_set(C):
+    '''
+    Tests whether complex number 'C' is in the set. 
+    returns number of iterations before escape. If C is in set,
+    returns 600.
+    '''
     Z = 0
     for iteration in range(1, 600):
         if abs(Z) > 2:
@@ -9,17 +18,28 @@ def in_set(C):
     return 600
 
 def get_complex(x, y, width, height):
+    '''
+    translates graph coordinates (x,y) in image size (width, height)
+    to complex number. Returns complex number.
+    '''
     return complex(-2 + 3.0/width * x, 1 - 2.0/height * y)
 
 def get_color(num):
+    '''
+    takes number and returns an HSV color. if number is greater 
+    than 600, returns black
+    '''
     if num < 600:
-        rgb = num  % 255
-        return (0, 0, int(rgb))
+        hsv = num  % (360 / 6)
+        return (int(hsv * 6), 360, 360)
     else:
         return (0, 0, 0)
 
 def render(width, height):
-    mandel = PIL.Image.new('RGBA', (width, height))
+    '''
+    given width and height of image, returns an image of mandelbrot set.
+    '''
+    mandel = PIL.Image.new('HSV', (width, height))
     for y in range(height):
         for x in range(width):
             C = get_complex(x, y, width, height)
@@ -27,4 +47,4 @@ def render(width, height):
             mandel.putpixel((x,y), color)
     mandel.show()
     
-render(2400,1600)
+render(1200,800)
