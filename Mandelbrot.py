2ca9a23407235afe5862a0de48884866a3c82139
Tansy Arron <tansy.arron@gmail.com>
2014-10-05 21:48:17 -0400

    Removed row reference and whitespace.

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,28 +1,23 @@
 import PIL.Image
 
 def in_set(C):
-	Z = 0
-	for iteration in range(1, 600):
-		if abs(Z) > 2:
-			return False
-		Z = Z ** 2 + C
-	return True
+    Z = 0
+    for iteration in range(1, 600):
+        if abs(Z) > 2:
+            return False
+        Z = Z ** 2 + C
+    return True
 
 def get_complex(x, y, width, height):
-	return complex(-2 + 3.0/width * x, 1 - 2.0/height * y)
+    return complex(-2 + 3.0/width * x, 1 - 2.0/height * y)
 
 def render(width, height):
-	mandel = PIL.Image.new('RGBA', (width, height))
-	for y in range(height):
-		row = []
-		for x in range(width):
-			C = get_complex(x, y, width, height)
-			if in_set(C):
-				mandel.putpixel((x,y), (255, 255, 255, 0))
-	mandel.show()
-
+    mandel = PIL.Image.new('RGBA', (width, height))
+    for y in range(height):
+        for x in range(width):
+            C = get_complex(x, y, width, height)
+            if in_set(C):
+                mandel.putpixel((x,y), (255, 255, 255, 0))
+    mandel.show()
+    
 print render(1200,800)
-
-
-
-
